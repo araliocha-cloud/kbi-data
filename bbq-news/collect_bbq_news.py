7826e3e1a8efb6b61a542de3624e7da7918e0881
araliocha-cloud <araliocha@gmail.com>
2026-09-21 12:01:01 +0900

    Update collect_bbq_news.py

--- bbq-news/collect_bbq_news.py
+++ bbq-news/collect_bbq_news.py
@@ -173,6 +173,8 @@ def parse_entry(entry, query_label, cache):
     url, alive = resolve_and_validate(raw_url, cache)
     if not alive:
         return None
+        if "news.google.com" in url:
+    return None
 
     published_struct = entry.get("published_parsed") or entry.get("updated_parsed")
     if published_struct:
